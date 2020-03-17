edb06409e1595e028e5e0c350270c0a28d617f29
samniss <58400608+samniss@users.noreply.github.com>
2020-03-17 21:26:06 +0200

    Create PolynomialSolver.java

---
+++ lab4/PolynomialSolver.java
@@ -0,0 +1,40 @@
+public class PolynomialSolver implements IPolynomialSolver {
+  private class term {
+    int coeff;
+    int exp;
+  }
+  @Override
+  public void setPolynomial(char poly, int[][] terms) {
+
+  }
+
+  @Override
+  public String print(char poly) {
+    return null;
+  }
+
+  @Override
+  public void clearPolynomial(char poly) {
+
+  }
+
+  @Override
+  public float evaluatePolynomial(char poly, float value) {
+    return 0;
+  }
+
+  @Override
+  public int[][] add(char poly1, char poly2) {
+    return new int[0][];
+  }
+
+  @Override
+  public int[][] subtract(char poly1, char poly2) {
+    return new int[0][];
+  }
+
+  @Override
+  public int[][] multiply(char poly1, char poly2) {
+    return new int[0][];
+  }
+}
