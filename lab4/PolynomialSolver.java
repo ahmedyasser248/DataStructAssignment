b106f6f8fd1f185142ab32420d8d40270fbd742b
samniss <58400608+samniss@users.noreply.github.com>
2020-03-18 02:51:24 +0200

    Update PolynomialSolver.java

--- lab4/PolynomialSolver.java
+++ lab4/PolynomialSolver.java
@@ -1,26 +1,84 @@
+package eg.edu.alexu.csd.datastructure;
+import eg.edu.alexu.csd.datastructure.DLinkedList.Node;
+import java.util.HashMap;
+import java.math.*;
 public class PolynomialSolver implements IPolynomialSolver {
+
+  public static void main(String[] args) {
+    PolynomialSolver test = new PolynomialSolver();
+    int[][] function = { {1,2},{1,4},{-5,6} };
+    test.setPolynomial('A',function);
+    test.print('A');
+    System.out.println(test.evaluatePolynomial('A',2));
+
+
+
+  }
   private class term {
     int coeff;
     int exp;
   }
+  public HashMap<Character, Node> polynomials=new HashMap<>();
+
+
   @Override
   public void setPolynomial(char poly, int[][] terms) {
+    DLinkedList newLL = new DLinkedList(); // new DLinkedList to store the polynomial expression
 
+
+    for (int[] term : terms) { //loop to go over the "terms" array and store it in a "term" object
+      term termTemp = new term();
+      termTemp.coeff = term[0];
+      termTemp.exp = term[1];
+      newLL.add(termTemp);
+    }
+
+    polynomials.put(poly,newLL.head);
   }
 
   @Override
   public String print(char poly) {
-    return null;
+    Node temp = polynomials.get(poly);
+    String expression;
+    term currentTerm = (term) temp.element;
+    if(currentTerm.coeff==1)
+      expression = "x^"  + currentTerm.exp;
+    else
+      expression = currentTerm.coeff + "x^"  + currentTerm.exp;
+    temp = temp.next;
+    while (temp != null){
+      currentTerm = (term) temp.element;
+      if (currentTerm.coeff<0){
+        expression += currentTerm.coeff + "x^"  + currentTerm.exp  ;
+      }
+      else if(currentTerm.coeff==1){
+        expression += "+" +  "x^"  + currentTerm.exp;
+      }
+      else {
+        expression += "+" + currentTerm.coeff + "x^"  + currentTerm.exp;
+      }
+      temp = temp.next;
+    }
+    System.out.println(expression);
+    return expression;
   }
 
   @Override
   public void clearPolynomial(char poly) {
-
+    polynomials.remove(poly);
   }
 
   @Override
   public float evaluatePolynomial(char poly, float value) {
-    return 0;
+    double result = 0;
+    Node temp = polynomials.get(poly);
+    term currentTerm;
+    while (temp != null){
+      currentTerm = (term) temp.element;
+      result += currentTerm.coeff * Math.pow(value,currentTerm.exp) ;
+      temp = temp.next;
+    }
+    return (float) result;
   }
 
   @Override
