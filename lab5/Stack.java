49b4eff8e44c8cfa07e4bda32fd47e7c6d0ffcd1
samniss <58400608+samniss@users.noreply.github.com>
2020-04-02 20:10:50 +0200

    Add files via upload

---
+++ lab5/Stack.java
@@ -0,0 +1,42 @@
+package eg.edu.alexu.csd.datastructure.stack;
+
+import eg.edu.alexu.csd.datastructure.linkedlist.DLinkedList;
+import java.util.EmptyStackException;
+
+public class Stack implements IStack{
+  DLinkedList stack = new DLinkedList();
+  int size = 0;
+  @Override
+  public Object pop() {
+    if(size==0)
+      throw new EmptyStackException();
+    Object data = stack.get(size-1);
+    stack.remove(--size);
+    return data;
+  }
+
+  @Override
+  public Object peek() {
+    if(size==0)
+      throw new EmptyStackException();
+    return stack.get(size-1);
+  }
+
+  @Override
+  public void push(Object element) {
+    stack.add(element);
+    size++;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    if(size == 0)
+      return true;
+    return false;
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+}
